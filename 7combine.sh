a2d5c8339291672118d5f6b392d4ae4fe48cdb1f
Derek <dohler@gmail.com>
2012-05-24 22:38:52 +0400

    Combine script now copies files instead of moving

--- 7combine.sh
+++ 7combine.sh
@@ -5,13 +5,13 @@ LANG=kat
 #for file in *.png; do
 #	filename=$(basename $file)
 #	filename=${filename%.*}
-mv normproto $LANG.normproto
-mv pffmtable $LANG.pffmtable
-mv Microfeat $LANG.Microfeat
-mv inttemp $LANG.inttemp
-mv shapetable $LANG.shapetable
-mv font_properties $LANG.font_properties
-mv unicharset $LANG.unicharset
+cp normproto $LANG.normproto
+cp pffmtable $LANG.pffmtable
+cp Microfeat $LANG.Microfeat
+cp inttemp $LANG.inttemp
+cp shapetable $LANG.shapetable
+cp font_properties $LANG.font_properties
+cp unicharset $LANG.unicharset
 
 combine_tessdata $LANG.
 #done
